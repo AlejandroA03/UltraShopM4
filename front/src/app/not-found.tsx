9b9094e544e5707fcc1cea8e3c92d4bbcaf1e143
Alejandro <a-a-m2001@hotmail.com>
2024-04-15 17:47:03 -0500

    deploy corrections

--- front/src/app/not-found.tsx
+++ front/src/app/not-found.tsx
@@ -5,7 +5,7 @@ export default function NotFound() {
         <div className="p-10 mt-24 mb-60 justify-around">
             <h1 className="text-3xl mx-10">oh...</h1>
             <h1 className="text-3xl mx-10 mt-10">It seems like something went wrong,</h1>
-            <h1 className="text-3xl mx-10">don't worry, you can use the navigation bar to get back on track!</h1>
+            <h1 className="text-3xl mx-10">You can use the navigation bar to get back on track!</h1>
         </div>
     );
   }
